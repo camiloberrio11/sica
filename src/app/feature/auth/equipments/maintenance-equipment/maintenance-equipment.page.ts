0c485ba6477ad5fb8ba8b89e4ff576497dd0b85f
Juan Camilo Berrio - Ceiba Software <camilo.berrio@ceiba.com.co>
2021-09-21 21:25:38 -0500

    create maintenance component

--- src/app/feature/auth/equipments/maintenance-equipment/maintenance-equipment.page.ts
+++ src/app/feature/auth/equipments/maintenance-equipment/maintenance-equipment.page.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormStep } from 'src/app/core/models/Formstep';
 
 @Component({
   selector: 'app-maintenance-equipment',
@@ -6,10 +7,28 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./maintenance-equipment.page.scss'],
 })
 export class MaintenanceEquipmentPage implements OnInit {
-
+  listItemsForm: FormStep[] = [{title: 'Equipo', status: true}, {title: 'Proveedor', status: false}];
+  indexCurrentForm = 0;
+  labelBtn = 'Continuar';
+  existNext = false;
   constructor() { }
 
   ngOnInit() {
+  }
+
+  handleNext() {
+    this.indexCurrentForm = this.indexCurrentForm + 1;
+
+    this.listItemsForm = this.listItemsForm.map(item => {
+      if (item?.title === this.listItemsForm[this.indexCurrentForm]?.title) {
+        item.status = true;
+      }
+      return item;
+    });
+    this.existNext = this.listItemsForm.filter(item => item?.status === true)?.length === this.listItemsForm.length;
+    if (this.existNext) {
+      this.labelBtn = 'Guardar';
+    }
   }
 
 }
