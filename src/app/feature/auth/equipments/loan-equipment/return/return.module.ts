bab7adc38ac1dbd97e508fd486ebcc2d131f1226
Juan Camilo Berrio - Ceiba Software <camilo.berrio@ceiba.com.co>
2021-09-23 21:02:03 -0500

    return component created

--- src/app/feature/auth/equipments/loan-equipment/return/return.module.ts
+++ src/app/feature/auth/equipments/loan-equipment/return/return.module.ts
@@ -7,13 +7,15 @@ import { IonicModule } from '@ionic/angular';
 import { ReturnPageRoutingModule } from './return-routing.module';
 
 import { ReturnPage } from './return.page';
+import { SharedModule } from 'src/app/shared/shared.module';
 
 @NgModule({
   imports: [
     CommonModule,
     FormsModule,
     IonicModule,
-    ReturnPageRoutingModule
+    ReturnPageRoutingModule,
+    SharedModule
   ],
   declarations: [ReturnPage]
 })
--- src/app/feature/auth/equipments/loan-equipment/return/return.page.ts
+++ src/app/feature/auth/equipments/loan-equipment/return/return.page.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormStep } from 'src/app/core/models/Formstep';
 
 @Component({
   selector: 'app-return',
@@ -6,10 +7,33 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./return.page.scss'],
 })
 export class ReturnPage implements OnInit {
-
+  listItemsForm: FormStep[] = [
+    { title: 'Equipo', status: true },
+    { title: 'Usuario', status: false },
+  ];
+  indexCurrentForm = 0;
+  labelBtn = 'Continuar';
+  existNext = false;
   constructor() { }
 
   ngOnInit() {
+  }
+
+  handleNext() {
+    this.indexCurrentForm = this.indexCurrentForm + 1;
+
+    this.listItemsForm = this.listItemsForm.map((item) => {
+      if (item?.title === this.listItemsForm[this.indexCurrentForm]?.title) {
+        item.status = true;
+      }
+      return item;
+    });
+    this.existNext =
+      this.listItemsForm.filter((item) => item?.status === true)?.length ===
+      this.listItemsForm.length;
+    if (this.existNext) {
+      this.labelBtn = 'Entregar';
+    }
   }
 
 }
